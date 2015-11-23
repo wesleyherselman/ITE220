5fc33215b1caa8aa00e1da0c2c9048c248afc05f
wesleyherselman <wesleyherselman@yahoo.com>
2015-11-23 10:14:51 +0700

    Update

--- Week2/js/add-content.js
+++ Week2/js/add-content.js
@@ -1,1 +1,10 @@
-document.write("Hello Week2!");
+var prive;
+var quantity;
+var total;
+
+price = 5;
+quantity =14;
+total = price * quantity;
+
+var totalPriceEle = document.getElementById("totalPrice");
+totalPriceEle.textContent = "$" + total;
