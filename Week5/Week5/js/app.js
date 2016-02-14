5df0ea15eb80d2526c3b60f90590ac0a4fb6778b
wesleyherselman <wesleyherselman@yahoo.com>
2016-02-14 14:58:47 +0700

    Week5

--- Week5/Week5/js/app.js
+++ Week5/Week5/js/app.js
@@ -16,31 +16,35 @@ $(function(){
 		},
 
 		init_event: function() {
-			$("#head_tagline>a:first").on("click",function()
-			{
-				$("#head_tagline>p:last").fadeIn(700);
+
+			$("#head_tagline>a:first").on("click",function(){
 				$("#head_tagline").height(320);
+				$("#head_tagline>p:last").fadeIn(700);
 			});
-			$("#head_tagline>a:last").on("click",function()
-			{
+			
+			$("#head_tagline>a:last").on("click",function(){
 				$("#head_tagline>p:last").hide();
 				$("#head_tagline").height(200);
 			});
 
-			$("#form-submit").on("click",function() {
+			$("#form-submit").on("click",function(){
 				var phone = $("#form-number").val();
 				if (!$.isNumeric(phone)) {
 					$(".error:first").show();
 				}
 				else{
 					$(".error:first").hide();
-				};
+				};					
 			});
 
 			$(".image-flip").flip({
 				axis: 'y',
-				trigger: 'hover'
+				trigger:'hover'
 			});
+
+
+
+
 		}
 
 	};
